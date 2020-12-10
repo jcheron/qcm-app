5a517fefd9e7a15594132ffc0387030497c0872f
Martin Jeanne <mjeanne@sio.local>
2020-12-10 12:01:20 +0100

    Dealing with database, seraching how to show data

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -10,7 +10,9 @@ use models\Question;
 use models\User;
 use Ubiquity\translation\TranslatorManager;
 use services\DashboardDAOLoader;
-
+use services\UserDAOLoader;
+use Exception;
+use PDO;
  /**
   * Controller DashboardController
   * @route('dashboard','automated'=>true)
@@ -18,23 +20,44 @@ use services\DashboardDAOLoader;
   */
 
 class DashboardController extends ControllerBase{
+    
     /**
-    *
-    * @autowired
-    * @var DashboardDAOLoader
-    */
+     *
+     * @autowired
+     * @var UserDAOLoader
+     */
     private $loader;
     
     /**
-    *
-    * @param \services\UserDAOLoader $loader
-    */
-    
+     *
+     * @param \services\UserDAOLoader $loader
+     */
     public function setLoader($loader) {
         $this->loader = $loader;
     }
     
+    private function pdo($session) {
+        try {
+            $bdd = new PDO("mysql:host=localhost;dbname=qcm;charset=utf8", "root", "");
+        }
+        
+        catch (Exception $e) {
+            die("Erreur : ".$e->get_Message());
+        }
+        
+        $sql = "SELECT caption FROM question WHERE idUser = " . $session['id'];
+        $req = $bdd->query($sql);
+        
+        $caption = array();
+        while ($donnees = $req->fetch()) {
+            array_push($caption, $donnees['caption']);
+        }
+        return $caption;
+    }
+    
     public function index() {
-        $this->loadView("DashboardController/index.html");
+        $session = USession::get('activeUser');
+        $caption = $this->pdo($session);
+        $this->loadView("DashboardController/index.html", ['caption' => $caption]) ;
     }
 }
